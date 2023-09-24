28e0d10d524b6a4cfb59fa9b0be9850d0456a33d
OurWinterium <144533418+OurWinterium@users.noreply.github.com>
2023-09-24 11:01:22 +0500

    Попытка сделать кнопку

--- Fragum Culturam.py
+++ Fragum Culturam.py
@@ -11,6 +11,25 @@ background = transform.scale(image.load("image.png"), (384, 384))
 mixer.music.load('Beyond the Lows - The Whole Other.mp3')
 mixer.music.play(-1)
 
+class Knopka(sprite.Sprite):
+    def __init__(self, sprite_image, spriteX, spriteY, sprite_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_image), (70, 30))
+        self.rect = self.image.get_rect()
+        self.rect.x = spriteX
+        self.rect.y = spriteY
+        self.speed = sprite_speed
+    def update(self):
+        self.rect.y += self.speed
+        self.rect.x += self.speed
+
+Knopki = sprite.Group()
+Knopka_1 = Knopka('a.bmp', 100, 100, 0)
+Knopki.add(Knopka_1)
+
+clock = time.Clock()
+FPS = 60
+
 game = True
 
 while game:
@@ -18,3 +37,8 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    Knopki.update()
+    Knopki.draw(window)
+    clock.tick(FPS)
+    display.update()
